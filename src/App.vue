e774e4012d313daeb046bb8b1b7f9d9a1565ac2d
John <89276080john@gmail.com>
2025-04-25 19:58:25 +0800

    Create Navbar.vue and update App.vue to use Navbar component

--- src/App.vue
+++ src/App.vue
@@ -1,51 +1,16 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import Navbar from './components/Navbar.vue' // 匯入 Navbar 組件
+import { ref } from 'vue'
+const companyName = ref('台中危老重建')
+const toggleName = () => {
+  companyName.value = companyName.value === '台中危老重建' ? 'TC Rebuild' : '台中危老重建'
+}
 </script>
 
 <template>
-  <!-- <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" /> -->
   <div>
     <!-- Bootstrap 導覽列 -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">台中危老重建</a>
-        <button
-          class="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarNav"
-          aria-controls="navbarNav"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
-        >
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" href="#">首頁</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">關於我們</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">案例展示</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">聯繫我們</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
+    <Navbar /> <!-- 使用 Navbar 組件 -->
 
     <!-- 主要內容 -->
     <div class="container mt-5">
@@ -53,21 +18,13 @@ import HelloWorld from './components/HelloWorld.vue'
       <p class="text-center">
         我們專注於提供專業的危老建築重建服務，打造安全、現代的居住環境。
       </p>
+      <button class="btn btn-primary" @click="toggleName">切換名稱</button>
     </div>
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
+.mt-5 {
+  margin-top: 3rem;
 }
 </style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,42 @@
+<script setup>
+// 目前無需邏輯，未來可加導覽列相關功能
+</script>
+
+<template>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#">台中危老重建</a>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" href="#">首頁</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">關於我們</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">案例展示</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">聯繫我們</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</template>
+
+<style scoped>
+/* 未來可加導覽列專屬樣式 */
+</style>
